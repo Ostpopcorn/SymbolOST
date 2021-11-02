1c7cabe14e9f0626032e4c3b5b9faf8aa559145c
Adrian Edin <adred130@student.liu.se>
2021-11-02 14:53:15 +0100

    Now also changes are commited xD

--- export_kicad_footprints.py
+++ export_kicad_footprints.py
@@ -6,13 +6,14 @@ lib_pretty_name = lib_name+".pretty"
 
 tag = "symbol"
 
+
 def get_path_to_lib():
     cwd = Path.cwd()
-    
-    if lib_name == cwd.name :
+
+    if lib_name == cwd.name:
         print("cwd")
         return cwd
-    
+
     # If not the cwd, check if the folder is a child to the cwd
     maybe_folder = cwd / lib_name
     if maybe_folder.exists() and maybe_folder.is_dir():
@@ -20,23 +21,26 @@ def get_path_to_lib():
         return maybe_folder
 
     # If not there, check its parrent first.
-    if cwd.parent.name == lib_name :#lib_name == os.path.pardir(path_of_the_directory):
+    # lib_name == os.path.pardir(path_of_the_directory):
+    if cwd.parent.name == lib_name:
         print("Parrent of cwd")
         return cwd.parent
-    raise FileExistsError("Cant find "+ lib_name +" Folder")
+    raise FileExistsError("Cant find " + lib_name + " Folder")
+
 
 def get_pretty_path(path_to_lib):
-    if isinstance(path_to_lib,str):
+    if isinstance(path_to_lib, str):
         path_to_lib = Path(path_to_lib)
     if not issubclass(type(path_to_lib), Path):
         raise TypeError("Given object is not a path")
-        
+
     pretty_folder = path_to_lib / lib_pretty_name
     if not (pretty_folder.exists() and pretty_folder.is_dir()):
         raise FileExistsError("Cant find "+lib_name+".pretty Folder")
     return pretty_folder
 
-def create_pretty_files(input_path,output_path, output_sizeses_mm = 0):
+
+def create_pretty_files(input_path, output_path, output_sizeses_mm=0):
     """
     If given input_path is a folder then all .svg files will be converted. Else only given .svg¨
     if given output_size_mm = 0 then the original dimension will be used (scale_factor = 1)
@@ -57,7 +61,8 @@ def create_pretty_files(input_path,output_path, output_sizeses_mm = 0):
             path = Path(path)
 
         if not path.exists():
-            raise FileExistsError("Input path:" + path.absolute() + " dose not exists.")
+            raise FileExistsError(
+                "Input path:" + path.absolute() + " dose not exists.")
         if path.is_dir():
             for subpath in path.iterdir():
                 if subpath.suffix == ".svg":
@@ -65,10 +70,11 @@ def create_pretty_files(input_path,output_path, output_sizeses_mm = 0):
         else:
             if subpath.suffix == ".svg":
                 files_to_convert.append(path)
-            
+
     if not output_path.exists():
-        raise FileExistsError("Output path:" + output_path.absolute() + " dose not exists.")
-    
+        raise FileExistsError(
+            "Output path:" + output_path.absolute() + " dose not exists.")
+
     # Here files_to_convert contains all files that should be processed.
     for svg in files_to_convert:
         split_name = svg.stem.split("_")
@@ -81,20 +87,23 @@ def create_pretty_files(input_path,output_path, output_sizeses_mm = 0):
         symbol_type = split_name[2]
         # Now construct command to pass to svg2mod
         for size in output_sizeses_mm:
-            if size >0:
+            if size > 0:
                 scale_factor = size/orig_size
                 size_str = str(size)
             else:
                 scale_factor = 1
                 size_str = str(orig_size)
-            co_path = output_path / (symbol_name + "_" + size_str +"mm_"+symbol_type + ".kicad_mod")
-            #svg2mod.exe -i $silk_screen_file_name --format pretty --factor ($size/$base_size) -c -o $save_path
-            cmd_str = "svg2mod -i " + str(svg.absolute()) + " --format pretty --factor " + str(scale_factor) + " -c -o " + str(co_path.absolute())
+            co_path = output_path / \
+                (symbol_name + "_" + size_str + "mm_"+symbol_type + ".kicad_mod")
+            # svg2mod.exe -i $silk_screen_file_name --format pretty --factor ($size/$base_size) -c -o $save_path
+            cmd_str = "svg2mod -i " + str(svg.absolute()) + " --format pretty --factor " + str(
+                scale_factor) + " -c -o " + str(co_path.absolute())
             os.system(cmd_str)
     pass
 
+
 def get_propper_path():
-    path_of_the_directory= os.getcwd()
+    path_of_the_directory = os.getcwd()
     directory_list = os.listdir()
     # Finding the folder
     if lib_name == os.path.basename(path_of_the_directory):
@@ -102,12 +111,12 @@ def get_propper_path():
         print("cwd")
     else:
         lib_folder_name = "lib"
-        lib_path = os.path.join(path_of_the_directory,lib_folder_name)
+        lib_path = os.path.join(path_of_the_directory, lib_folder_name)
         if not os.path.isdir(lib_path):
             lib_path = path_of_the_directory
         path_to_symbol_folder = ""
 
-        path_to_symbol_folder = os.path.join(lib_path,lib_name)
+        path_to_symbol_folder = os.path.join(lib_path, lib_name)
         print(path_to_symbol_folder)
         if not os.path.isdir(path_to_symbol_folder):
             raise FileNotFoundError("Could not locate dir...")
@@ -118,7 +127,7 @@ def main():
     # path_of_the_directory= 'E:\Python for Data Science'
     path_of_the_directory = get_propper_path()
     # step into pretty folder
-    
+
     # Here the folder is found
     for filename in os.listdir(pretty_folder):
         # replace Copper with cu
@@ -127,10 +136,11 @@ def main():
         # replace svg2mod with propper name
         # set a good tag (name up to first _ or to size marking (first number))
         # See how to remove ss from top of cu ones.
-        file_path = os.path.join(pretty_folder,filename)
-        output_file_path = file_path # os.path.join(pretty_folder,"out_"+filename)
-        #if os.path.isfile(f):
-        with open(file_path,'rt') as f:
+        file_path = os.path.join(pretty_folder, filename)
+        # os.path.join(pretty_folder,"out_"+filename)
+        output_file_path = file_path
+        # if os.path.isfile(f):
+        with open(file_path, 'rt') as f:
             text = f.read()
             # first replace the tag
 
@@ -139,18 +149,17 @@ def main():
         replacement_string = replacement_string.replace("Copper", "cu")
         tag = replacement_string.split("_")[0]
 
-        text = text.replace("tags svg2mod", "tags " + tag )
-        text = text.replace("svg2mod",replacement_string )
+        text = text.replace("tags svg2mod", "tags " + tag)
+        text = text.replace("svg2mod", replacement_string)
 
-        with open(output_file_path,'wt') as f:
+        with open(output_file_path, 'wt') as f:
             f.write(text)
-    
-    
+
 
 if __name__ == "__main__":
     path_to_lib = get_path_to_lib()
     path_to_pretty_folder = get_pretty_path(path_to_lib)
-    output_dims = [5,8,10,12,14,16,20,25]
-    created_files = create_pretty_files(path_to_lib,path_to_pretty_folder)
+    output_dims = [5, 8, 10, 12, 14, 16, 20, 25]
+    created_files = create_pretty_files(path_to_lib, path_to_pretty_folder)
 
     # main()
